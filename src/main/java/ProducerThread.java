94abd54e017e7a19fc03a38c88abc817a75680f6
AfonsoRod <2124322@student.uma.pt>
2025-03-27 15:35:46 +0000

    feat: adding sync to producer and consumer

--- src/main/java/ProducerThread.java
+++ src/main/java/ProducerThread.java
@@ -1,19 +1,34 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class ProducerThread extends Thread {
-    private final BlockingQueue<String> logQueue;
+    private final Queue<String> logQueue;
     private final String logEntry;
+    private final Semaphore semaphore;
+    private final ReentrantLock lock;
 
-    public ProducerThread(BlockingQueue<String> logQueue, String logEntry) {
+    public ProducerThread(Queue<String> logQueue, String logEntry, Semaphore semaphore, ReentrantLock lock) {
         this.logQueue = logQueue;
         this.logEntry = logEntry;
+        this.semaphore = semaphore;
+        this.lock = new ReentrantLock();
     }
+
 
     @Override
     public void run() {
         try {
-            logQueue.put(logEntry);
-        } catch (InterruptedException e) {
+            lock.lock();
+
+            logQueue.add(logEntry);
+
+            semaphore.release();
+            lock.unlock();
+        } catch (Exception e) {
             Thread.currentThread().interrupt();
         }
     }
